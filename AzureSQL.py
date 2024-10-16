1c08c90e23d27bd2fc6e59e2db79133fd0190071
AlexLebada <alex.lebada@yahoo.com>
2024-10-16 12:58:08 +0300

    3rd commit

--- AzureSQL.py
+++ AzureSQL.py
@@ -1,4 +1,20 @@
-import pypyodbc as odbc
+import pyodbc
 import pandas as pd
-from credential import username, password
+#from credential import username, password
 
+server = 'tcp:testtest123123.database.windows.net'
+database = 'test'
+username = 'alexlebada'
+password = 'Happyness123@'
+connection_string = 'DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+', 1433;DATABASE='+database+';UID='+username+';PWD='+password+';ENCRYPT=yes;TrustServerCertificate=no;Connection Timeout=30;'
+conn = pyodbc.connect(connection_string)
+
+conn.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+conn.execute("""INSERT INTO customers (name, address) VALUES
+('Mike', 20),
+('John', 32),
+('Sanda', 35),
+('Julian', 40),
+('Joanna', 50)
+""")
+conn.commit()
